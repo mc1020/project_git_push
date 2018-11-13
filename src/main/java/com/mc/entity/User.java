0360c210c0510206a77a3cc88544f763e75c18c1
mc <1330963698@qq.com>
2018-11-13 21:49:02 +0800

    add date

--- src/main/java/com/mc/entity/User.java
+++ src/main/java/com/mc/entity/User.java
@@ -1,10 +1,12 @@
 package com.mc.entity;
 
+import java.util.Date;
+
 public class User {
     private String id;
     private String username;
     private String password;
-
+    private Date date;
     public User() {
     }
 
